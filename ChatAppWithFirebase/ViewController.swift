31e4ab0a886f7a3881ba39c1f5820977691acd31
takuro358 <63958348+takuro358@users.noreply.github.com>
2020-07-08 19:03:54 +0900

    Initial Commit

---
+++ ChatAppWithFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by takuro on 2020/07/08.
+//  Copyright © 2020 takuro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
